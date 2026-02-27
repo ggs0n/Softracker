8db4d96bf8aaf1e772443162ae9e053c9355deea
ggs0n <adibafifisaadon@gmail.com>
2026-02-27 18:49:53 +0800

    fix: validate task timeline end date is not before start

--- WFHMonitor/ViewModels/TaskViewModels.cs
+++ WFHMonitor/ViewModels/TaskViewModels.cs
@@ -3,7 +3,7 @@ using WFHMonitor.Models;
 
 namespace WFHMonitor.ViewModels;
 
-public class TaskCreateViewModel
+public class TaskCreateViewModel : IValidatableObject
 {
     [Required, MaxLength(200)]
     public string Title { get; set; } = string.Empty;
@@ -35,6 +35,16 @@ public class TaskCreateViewModel
 
     [Display(Name = "Related Bug")]
     public int? BugReportId { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (TimelineStart.HasValue && TimelineEnd.HasValue && TimelineEnd.Value.Date < TimelineStart.Value.Date)
+        {
+            yield return new ValidationResult(
+                "Timeline End must be on or after Timeline Start.",
+                new[] { nameof(TimelineEnd) });
+        }
+    }
 }
 
 public class TaskEditViewModel : TaskCreateViewModel
